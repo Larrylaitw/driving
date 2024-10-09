273e71902491a9aa74e4ff96edb270f377a0a32a
Larry La <lailarry1226@gmail.com>
2024-10-09 15:30:34 +0800

    add more smart

--- driving.py
+++ driving.py
@@ -1,12 +1,14 @@
 country = input('請問你是台灣或美國人:')
-age = input('輸入你的年齡: ')
-age = int(age)
 if country == 'Taiwan':
+    age = input('輸入你的年齡: ')
+    age = int(age)
     if age >= 18:
         print('you can test license')
     else:
         print('You can not test')
 elif country == 'US':
+    age = input('輸入你的年齡: ')
+    age = int(age)
     if age >= 16:
         print('you can test license')
     else:
